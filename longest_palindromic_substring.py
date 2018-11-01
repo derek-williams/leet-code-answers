03f0500b383d878110b41ff60078d3ea0aaf3bc9
Derek Williams <derek@bodysnatchers.studio>
2018-11-01 14:59:57 -0700

    starting a new problem

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,8 @@
+#Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
